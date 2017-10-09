77d42a91fe9eb24001b4c0bb7ee612e472a0506d
Gabriel Malet <gabrielmalet@gmail.com>
2017-10-09 11:31:02 +0200

    Add ssh support

--- alpine-java-8/resources/entrypoint.sh
+++ alpine-java-8/resources/entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# Check private key
+if [ -z "$SSH_PRIVATE_KEY" ]; then
+	echo >&2 'error: missing SSH_PRIVATE_KEY variable'
+	exit 1
+fi
+
+eval $(ssh-agent -s)
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+unset SSH_PRIVATE_KEY
+
+# Replace nexus variables
 sed -i \
     -e "s/##deployer_user##/${NEXUS_USER}/g" \
     -e "s/##deployer_pwd##/${NEXUS_PASSWORD}/g" \
